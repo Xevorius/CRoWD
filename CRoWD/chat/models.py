802b3133fec74affc63bd52975d024b2b2638f7e
TimRietdijk <t.rietdijk@st.hanze.nl>
2023-11-06 15:49:39 +0100

    Folder restructure for PA

--- CRoWD/chat/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Chat(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
--- CRoWD/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoute),
-    path('chats/', views.get_chats),
-    path('chats/<str:pk>/', views.get_user_chats),
-]
-
