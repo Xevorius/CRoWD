3d6637f5729213e3746485946cc66200953ed6fd
TimRietdijk <t.rietdijk@st.hanze.nl>
2023-11-08 10:02:39 +0100

    First CRUD operations test

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.getRoute),
     path('chats/', views.get_chats),
+    path('chats/create/', views.create_chat),
     path('chats/<str:pk>/', views.get_user_chats),
+    path('chats/<str:pk>/delete', views.delete_chat),
 ]
 
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 from .models import Chat
 from .serializers import ChatSerializer
@@ -21,3 +22,18 @@ def get_user_chats(requests, pk):
     chats = Chat.objects.filter(users=pk)
     serializer = ChatSerializer(chats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_chat(requests): 
+    data = requests.data
+    chat = Chat.objects.create(users=User.objects.get(pk=data['users']))
+    serializer = ChatSerializer(chat, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def delete_chat(request, pk):
+    chat = Chat.objects.get(pk=pk)
+    chat.delete()
+    return Response('Chat was deleted')
